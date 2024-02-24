a3552565a763ae6e68b08d4c78f498421ad9ff36
BernardBackup <160973663+BernardBackup@users.noreply.github.com>
2024-02-24 22:39:53 +0100

    Update Header.tsx
    Header

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 
 export default function Header() {
-  const [open, setOpen] = React.useState(false);
+
 
 
   return (
