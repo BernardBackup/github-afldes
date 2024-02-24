c9dc37d5a234598c0dff4273c6a55caa6191c6f0
BernardBackup <160973663+BernardBackup@users.noreply.github.com>
2024-02-24 23:17:51 +0100

    Update index.tsx
    h-120px

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@
  */
  import { trpc } from '../utils/trpc';
 //import Header from '../components/Header'
+import lambdablack from '../../assets/images/LAMBDA_black.svg';
+import lambdablabel from '../../assets/images/LAMBDA_label.svg';
 
  export default function IndexPage() {
    // Define an array to store image components
@@ -37,8 +39,8 @@
         <nav className={`  top-[27px] fixed  nav-bg-top w-[91%] 3xl:max-w-[1836px] z-[99] rounded-[60px] px-[16px] sm:px-2 md:px-8 mx-auto `}  >
           <div className="   flex flex-wrap items-center justify-between mx-auto py-3">
             <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
-  {/*<img src={Logo} className=" w-[48px] h-[48px] " alt="Flowbite Logo" />*/}
-              <span className="self-center f-f-sm text-tiny  whitespace-nowrap text-white ">Bernard Degens</span>
+              <img src={lambdablack} className=" w-[48px] h-[48px] " alt="Flowbite Logo" />
+               <img src={lambdablabel} className=" w-[48px] h-[120px] " alt="Flowbite Logo" />
             </a>
             {/*<button data-collapse-toggle="navbar-default" type="button" className=" mr-3 inline-flex items-center  justify-center text-sm  md:hidden " aria-controls="navbar-default" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
